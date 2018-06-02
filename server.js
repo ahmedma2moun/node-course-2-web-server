49ebd106e7b08b76d4936848a4523e088031ecc4
gpteamcs <ahmedfcisasu@gmail.com>
2018-06-02 11:14:11 +0200

    Add projects page

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.get('/about', (req, res) => {
     })
 });
 
+app.get('/projects', (req, res) => {
+    res.render('project.hbs', {
+        pageTitle: "Project Page"
+    })
+});
+
 
 app.get('/bad', (req, res) => {
     res.send({
